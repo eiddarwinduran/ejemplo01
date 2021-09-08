329a04d2777b1fd8f387ea4b2b82f7d3b3376f39
ASUS <ASUS@LAPTOP-M884M9NN>
2021-09-08 18:49:28 +0200

    Agregar archivos de proyecto.

---
+++ ejemplo01/principal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Apellidos Nombre: Duran Condori Eid Darwin" <<std::endl;
+	std::cout << "Carrera: Ing De Sistemas" << std::endl;
+	std::cout << "CU: 35-5117" << std::endl;
+	std::cout << "Correo electronico: eiddarwind734@gmail.com" << std::endl;
+	return 0;
+}
